f8767a67a55848c0e6cb12209b4eba11a6bb3801
christyzhang11 <yuangvoice@gmail.com>
2016-09-04 13:35:56 -0700

    basic function is ready

--- chatroom/js/main.js
+++ chatroom/js/main.js
@@ -2,62 +2,69 @@ $(function(){
     var socket = io();
     //var connected = false;
 
-    //type enter to submit
-    $("input").keydown(function(event) {
-        if (event.which == 13 ) {
-        	if(socket.username){
-            	$("#post").click();
-        	}else{
-        		$("#submit").click();
-       	 	}
-       	}
-    });
-
     $(".form").submit(function(){
-    	post();
-    	return false;
+        post();
+        return false;
     });
 
     $("#submit").click(function(){
-    	getUserName();
-    	return false;
+        getUserName();
+        return false;
     });
 
-    socket.on('chat message', function(msg){
-        newMessage(msg)
+    socket.on('chatMessageSuccess', function(message) {
+      newMessage(message);
+    });
+
+    socket.on('userLeft', function(data) {
+        var msg = $('<span class="left">').text(data.username + " left the chat room.");
+        $("#messages").append($('<li>')).append(msg);
     });
 
     function getUserName() {
-    	socket.username = $("#username").val().trim();
-    	if(socket.username) {
-    		$("#login").fadeOut();
-    		$("#chatroom").show();
-    		//emit add user name action
-    		socket.emit('add user', socket.username);
-    	}else{
-    		alert("invalid username");
-    	}
+        socket.username = $("#username").val().trim();
+        if(socket.username) {
+            $("#login").fadeOut();
+            $("#chatroom").show();
+
+          //emit add user name action
+          socket.emit('addUser', socket.username);
+          socket.on('addUserSuccess', function(data) {
+              for(var i = 0; i < data.length; i++) {
+                newMessage(data[i]);
+              }
+          });
+
+          //notify all user 
+          socket.on('welcome', function(msg){
+            newMessage(msg);
+          });
+        }else{
+            alert("invalid username");
+        }
     }
 
     function post() {
-    	var msg = $('#m').val().trim();
-    	//emit post message action
-    	socket.emit('chat message', {
-    		username : socket.username,
-    		message: msg 
-    	});
-    	//reset the input
-    	$('#m').val('');
+        var msg = $('#m').val().trim();
+        //emit post message action
+        socket.emit('chatMessage', {
+            username : socket.username,
+            message: msg,
+            time: new Date()
+        });
+
+        //reset the input
+        $('#m').val('');
     }
 
     function newMessage(data){
-    	formNewMessage(data);
+        formNewMessage(data);
     }
 
     function formNewMessage(data){
-    	var user = $('<span class="postuser" />').text(data.username);
-    	var time = $('<span class="posttime" />').text(data.time);
-    	var msg = $('<span class="msgbody"/>').text(data.message);
-    	$("#messages").append($('<li>')).append(user, msg, time);
+        var user = $('<span class="postuser" />').text(data.username);
+        var time = $('<span class="posttime" />').text(data.timestamp);
+        var msg = $('<span class="msgbody"/>').text(data.message);
+        $("#messages").append($('<li>')).append(user, msg, time);
     }
 });
--- chatroom/server.js
+++ chatroom/server.js
@@ -9,86 +9,66 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //db
-
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
-//initialize
-var dbexist = false;
-
-db.serialize(function() {
-	if(!dbexist) {
-		db.run("CREATE TABLE MESSAGE (username TEXT, message TEXT, timestamp DATETIME)");
-	}
-})
+var db = new sqlite3.Database('test.db');
 
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   //post a new message
-  socket.on('chat message', function(data){
-  	var date = timeUtil();
-  	var message = {
-  		username: data.username,
-  		message: data.message,
-  		time: date
-  	};
-  	saveMessage(message);
-    io.emit('chat message', message);
+  socket.on('chatMessage', function(data){
+    saveMessage(data);
+    data.timestamp = timeUtil();
+    io.emit('chatMessageSuccess', data);
   });
 
   //add a new user
-  socket.on('add user', function(username){
-  	var date = timeUtil();
-  	var message = {
-  		username: "Admin",
-  		message: "Welcome " + username + " to FSE chatroome",
-  		time: date
-  	};
-  	io.emit('chat message', message);
-  	//then load old messages
-  	loadMessage(username);
+  socket.on('addUser', function(username){
+    socket.username = username;
+    // load previous messages
+    db.all("SELECT username, message, timestamp FROM MESSAGE", function(err, rows){
+      if(err === null) {
+        socket.emit('addUserSuccess', rows); //just notify the joined user
+      }else {
+        // return customed error message
+      }
+    });
+     var welcomeMessage = {
+      username: "Admin",
+      message: "Welcome " + username + " to FSE chatroom",
+      timestamp: timeUtil()
+    };
+
+    io.emit("welcome", welcomeMessage);
+  });
+
+  //user left
+  socket.on('disconnect', function(){
+    io.emit('userLeft', {
+      username : socket.username
+    });
   })
 
 });
 
 http.listen(3000, function(){
-	console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
-
 
 //a util function to format current time
 function timeUtil(){
   var timestr = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-  return timestr;   
-}
-
-//db operations
-
-function loadMessage(username) {
-	db.all("SELECT username, message, timestamp FROM MESSAGE", function(err, rows){
-		// socket.broadcast.to(username).emit('chat message', {
-		// 	username: username,
-  // 			message: message,
-  // 			time: timestamp
-		// });
-	console.log(rows.username, rows.message, rows.timestamp);
-	});
+  return timestr;
 }
 
 function saveMessage(data) {
-	db.serialize(function(){
-		var stmt = db.prepare("INSERT INTO MESSAGE VALUES(" +data.username +"," + data.message +
-			"," + data.time);
-		console.log(stmt);
-		stmt.run();
-		stmt.finalize();
-	});
+  db.serialize(function(){
+    var stmt = db.prepare("INSERT INTO MESSAGE(username, message) VALUES('" +data.username +"','" + data.message + "')");
+    stmt.run();
+    stmt.finalize();
+  });
 }
-
-
-
 
